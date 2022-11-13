38890ab38d8978649c7a1c5c8f6e9aaf460cb675
AlexDstart <demyanalex@gmail.com>
2022-11-13 18:59:04 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Задача 2");
         var dog = 15;
         System.out.println(dog);
+        System.out.println("Задача 3");
+        var cucmber = 15;
+        System.out.println(cucmber);
     }
 }
